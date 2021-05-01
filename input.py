db7064b43f9d5783ca61f47d7f47a6337485e62a
Shashank Thanalapati <shashank_thanalapati@sfu.ca>
2021-05-01 11:32:59 -0700

    Added a few more buttons including start and select

--- input.py
+++ input.py
@@ -1,18 +1,23 @@
-from pynput.keyboard import Controller
+from pynput.keyboard import Controller, Key
+import time
 
 keyboard = Controller()
 
 
 def sendInput(input):
+    time.sleep(2)
     inputCharacter = input.lower()
-
-    if inputCharacter == 'w' or inputCharacter == 'W':
+    if inputCharacter == "start":
+        keyboard.press(Key.enter)
+    if inputCharacter == "select":
+        keyboard.press(Key.shift_r)
+    if inputCharacter == 'w':
         keyboard.type('w')
-    if inputCharacter == 'a' or inputCharacter == 'A':
+    if inputCharacter == 'a':
         keyboard.type('a')
-    if inputCharacter == 's' or inputCharacter == 'S':
+    if inputCharacter == 's':
         keyboard.type('s')
-    if inputCharacter == 'd' or inputCharacter == 'D':
+    if inputCharacter == 'd':
         keyboard.type('d')
     if inputCharacter == '6':  # A button
         keyboard.type('6')
@@ -22,3 +27,10 @@ def sendInput(input):
         keyboard.type('8')
     if inputCharacter == '4':  # Y button
         keyboard.type('4')
+    if inputCharacter == '7':  # L1 button
+        keyboard.type('7')
+    if inputCharacter == '9':  # R1 button
+        keyboard.type('9')
+
+
+sendInput("start")
