bd60947082187750588246490bbc203b62d50362
Gregwatt <gregwattdev@outlook.com>
2021-05-01 14:37:32 -0700

    Switch input to use PyDirectInput, and now the emulator can receive the controls!

--- input.py
+++ input.py
@@ -1,57 +1,55 @@
 from pynput.keyboard import Controller, Key
 import time
-
+import pyautogui
+import pydirectinput
 keyboard = Controller()
 
 
 def sendInput(input):
-    time.sleep(2)
     inputCharacter = input.lower()
     if inputCharacter == "start":
-        keyboard.press(Key.enter)
+        pydirectinput.press('enter')
     if inputCharacter == "select":
-        keyboard.press(Key.shift_r)
+        pydirectinput.press('shiftright')
     if inputCharacter == 'f1':  # RetroArch Menu button
-        keyboard.press('Key.f1')
+        pydirectinput.press('f1')
     if inputCharacter == 'up':
-        keyboard.type('w')
+        pydirectinput.press('w')
     if inputCharacter == 'left':
-        keyboard.type('a')
+        pydirectinput.press('a')
     if inputCharacter == 'down':
-        keyboard.type('s')
+        pydirectinput.press('s')
     if inputCharacter == 'right':
-        keyboard.type('d')
+        pydirectinput.press('d')
 
     if inputCharacter == 'a':  # A button
-        keyboard.type('6')
+        pydirectinput.press('6')
     if inputCharacter == 'b':  # B button
-        keyboard.type('2')
+        pydirectinput.press('2')
     if inputCharacter == 'x':  # X button
-        keyboard.type('8')
+        pydirectinput.press('8')
     if inputCharacter == 'y':  # Y button
-        keyboard.type('4')
+        pydirectinput.press('4')
 
     if inputCharacter == 'l1':  # L1 button
-        keyboard.type('7')
+        pydirectinput.press('7')
     if inputCharacter == 'l2':  # R1 button
-        keyboard.type('9')
+        pydirectinput.press('9')
     if inputCharacter == 'l2':  # L2 button
-        keyboard.type('o')
+        pydirectinput.press('o')
     if inputCharacter == 'r2':  # R2 button
-        keyboard.type('p')
+        pydirectinput.press('p')
     if inputCharacter == 'l3':  # L3 button
-        keyboard.type('k')
+        pydirectinput.press('k')
     if inputCharacter == 'r3':  # R3 button
-        keyboard.type('l')
+        pydirectinput.press('l')
 
     if inputCharacter == 'anu':  # Analog Up button
-        keyboard.type('b')
+        pydirectinput.press('b')
     if inputCharacter == 'and':  # Analog Down button
-        keyboard.type('n')
+        pydirectinput.press('n')
     if inputCharacter == 'anl':  # Analog Left button
-        keyboard.type('m')
+        pydirectinput.press('m')
     if inputCharacter == 'anr':  # Analog Right button
-        keyboard.type(',')
+        pydirectinput.press(',')
 
-
-sendInput("start")
--- main.py
+++ main.py
@@ -2,12 +2,25 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import io
 
 import discord
 import json
+import os
+import subprocess
+import threading
+import time
+
+from PIL import ImageGrab
+import win32gui
+from discord.ext import tasks
+from input import sendInput
+from retroarchpythonapi import RetroArchPythonApi as RetroAPI
 
 client = discord.Client()
 
+retroarchOpen = False
+retroarchProcess = None
 
 @client.event
 async def on_ready():
@@ -16,6 +29,9 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    global retroarchOpen
+    global retroarchProcess
+
     if message.author == client.user:
         return
 
@@ -25,6 +41,111 @@ async def on_message(message):
     if message.content.startswith('$startGame'):
         response = "Starting game" + message.content[10::1]
         await message.channel.send(response)
+
+    if message.content.startswith('emuStart'):
+        if retroarchOpen:
+            await message.channel.send('Retroarch was already started!')
+        else:
+            retroarchProcess = subprocess.Popen('./retroarch/retroarch.exe')
+            if process_exists('retroarch.exe'):
+                retroarchOpen = True
+
+                time.sleep(1)
+                sendInput("left")
+                sendInput("down")
+                sendInput("down")
+                sendInput("down")
+                sendInput("right")
+
+                send_screenshot.start(message.channel)
+                await message.channel.send('Started Retroarch!')
+            else:
+                await message.channel.send('Retroarch was not started...')
+
+    if message.content.startswith('emuStop'):
+        if retroarchOpen:
+            retroarchProcess.terminate()
+            send_screenshot.stop()
+            retroarchOpen = False
+            await message.channel.send('Stopped Retroarch!')
+        else:
+            await message.channel.send('No Retroarch process to stop...')
+
+    if message.content.lower().startswith('start') and retroarchOpen:
+        sendInput("start")
+    if message.content.lower().startswith('select') and retroarchOpen:
+        sendInput("select")
+    if message.content.lower().startswith('up') and retroarchOpen:
+        sendInput("up")
+    if message.content.lower().startswith('down') and retroarchOpen:
+        sendInput("down")
+    if message.content.lower().startswith('left') and retroarchOpen:
+        sendInput("left")
+    if message.content.lower().startswith('right') and retroarchOpen:
+        sendInput("right")
+    if message.content.lower().startswith('a') and retroarchOpen:
+        sendInput("a")
+    if message.content.lower().startswith('b') and retroarchOpen:
+        sendInput("b")
+    if message.content.lower().startswith('x') and retroarchOpen:
+        sendInput("x")
+    if message.content.lower().startswith('y') and retroarchOpen:
+        sendInput("y")
+    if message.content.lower().startswith('l1') and retroarchOpen:
+        sendInput("l1")
+    if message.content.lower().startswith('r1') and retroarchOpen:
+        sendInput("r1")
+    if message.content.lower().startswith('l2') and retroarchOpen:
+        sendInput("l2")
+    if message.content.lower().startswith('r2') and retroarchOpen:
+        sendInput("r2")
+    if message.content.lower().startswith('l3') and retroarchOpen:
+        sendInput("l3")
+    if message.content.lower().startswith('r3') and retroarchOpen:
+        sendInput("r3")
+    if message.content.lower().startswith('anu') and retroarchOpen:
+        sendInput("anu")
+    if message.content.lower().startswith('and') and retroarchOpen:
+        sendInput("and")
+    if message.content.lower().startswith('anl') and retroarchOpen:
+        sendInput("anl")
+    if message.content.lower().startswith('anr') and retroarchOpen:
+        sendInput("anr")
+
+def process_exists(process_name):
+    call = 'TASKLIST', '/FI', 'imagename eq %s' % process_name
+    # use buildin check_output right away
+    output = subprocess.check_output(call).decode()
+    # check in last line for process name
+    last_line = output.strip().split('\r\n')[-1]
+    # because Fail message could be translated
+    return last_line.lower().startswith(process_name.lower())
+
+
+@tasks.loop(seconds=1.0)
+async def send_screenshot(channel):
+    # take a screenshot and save to disk
+    toplist, winlist = [], []
+
+    def enum_cb(hwnd, results):
+        winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+
+    win32gui.EnumWindows(enum_cb, toplist)
+
+    retroarch = [(hwnd, title) for hwnd, title in winlist if 'retroarch' in title.lower()]
+    # just grab the hwnd for first window matching retroarch
+    retroarch = retroarch[0]
+    hwnd = retroarch[0]
+
+    # win32gui.SetForegroundWindow(hwnd)
+    bbox = win32gui.GetWindowRect(hwnd)
+    img = ImageGrab.grab(bbox)
+    arr = io.BytesIO()
+    img.save(arr, format='PNG')
+    arr.seek(0)
+    file = discord.File(arr, 'a.png')
+    # send screenshot to channel
+    await channel.send(file=file)
 
 
 with open('token.json') as json_file:
